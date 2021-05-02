16ed29e13e3078646bcf897a62bd9125b350c0c3
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-01 20:16:57 -0500

    Find the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    number *= -1
-    last = (number % 10) * -1
+    last = (((number * -1) % 10) * -1)
 else:
     last = (number % 10)
 str = "Last digit of {:d} is {:d}".format(number, last)
