543481c6c6478d11dded5516f21dcfb2fe40ea7d
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-01 23:35:47 -0500

    Is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for c in ord(range(97, 123)):
-        if c >= 97 and c <= 122:
-            return True
+    if ord(c >= 97 and c <= 122):
+        return True
+    else:
         return False
