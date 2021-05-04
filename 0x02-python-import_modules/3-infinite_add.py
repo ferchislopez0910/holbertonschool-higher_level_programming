88ffcd68da47673a2cf3391acbc06d500642e53d
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-03 22:49:35 -0500

    sum infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,11 @@
 if __name__ == "__main__":
     import sys
     ln = len(sys.argv)
-
-    for i in ln:
-        sum += i(int(sys.argv))
-        print(sum)
+    argument = (sys.argv)
+    if argument == 1:
+        print('0')
+    else:   
+        sum = 0
+        for i in (1, ln):
+            sum += (int(sys.argv[i]))
+        print('{}'.format(sum))
