2284bcd8674396bb33e1bec3c3ba4245256cf40e
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-11 18:22:20 -0500

    Update

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            i = "FizzBuzz "
+            print(i, end=" ")
+        elif i % 3 == 0:
+            i = 'Fizz'
+            print(i, end=" ")
+        elif i % 5 == 0:
+            i = "Buzz"
+            print(i, end='')
+        print(" ", i, end=" ")
