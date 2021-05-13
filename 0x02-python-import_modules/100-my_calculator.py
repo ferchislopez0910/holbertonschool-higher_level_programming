6183e2231befd6c6d89704654089a126bfe600ff
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-13 12:06:18 -0500

    Advance 1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = ['+', '-', '*', '/']
+    from calculator_1 import add, sub, mul, div
+    operactions = [add, sub, mul, div]
+    for i, j in enumerate(operator):
+        if argv[2] == j:
+            print("{} {} {} = {}".format(a, j, b, operactions[i](a, b)))
+            break
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
