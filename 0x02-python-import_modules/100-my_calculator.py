096a4f2d80d7d946d0b96a991fd13c28de675704
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-13 12:11:26 -0500

    Advance 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
     a = int(argv[1])
     b = int(argv[3])
     operator = ['+', '-', '*', '/']
