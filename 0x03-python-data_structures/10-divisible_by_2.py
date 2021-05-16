16e1be9ab344b0b22ff1c354732fb60009f21bda
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-16 08:54:18 -0500

    Function that is %2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list
+    new_list = list(my_list)
     for i in new_list:
         if i % 2 == 0:
-            return (True)
-        return(False)
+            new_list[i] = True
+        else:
+            new_list[i] = False
+    return new_list
