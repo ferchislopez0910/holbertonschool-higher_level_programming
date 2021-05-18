79cf28ca8e0f6fb5c8c6e0d9085057df68601a7a
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-18 17:58:54 -0500

    Def area and multipli por slide

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""def class Square that defines a square"""
+
+
+class Square:
+    """empty class Square that
+
+    defines a square"""
+    def __init__(self, size=0):
+        """Created a module"""
+        if isinstance(size, int):
+            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Def del area"""
+        return self.__size ** 2
