309e574da3426c418b74b28afb18332f2d81a35f
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-19 17:16:19 -0500

    Advance - print integrer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        print('{:d}'.format(value))
         return True
     except Exception as e:
-        print("Exception:{}".format(e), file=sys.stderr)
+        print('Exception: {}'.format(e), file=sys.stderr)
         return False
