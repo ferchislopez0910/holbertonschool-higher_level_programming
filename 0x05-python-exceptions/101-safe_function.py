3b2ceeb3add644da5d953080bbb5ae23e48a1caf
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-19 17:34:26 -0500

    Print error looks  like pointers

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        funct_safe = fct(*args)
+        return funct_safe
+    except Exception as e:
+        print('Exception: {}'.format(e), file=sys.stderr)
+        return False
