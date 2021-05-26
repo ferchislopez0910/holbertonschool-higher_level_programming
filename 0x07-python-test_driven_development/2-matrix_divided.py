daa0eee4ad83b0751ffc2ee19561de420daaf36d
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-26 09:10:22 -0500

    Update **n: add if statment with is not matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,14 +15,14 @@ def matrix_divided(matrix, div):
     x = "matrix must be a matrix (list of lists of integers/floats"
     y = "matrix must be a matrix (list of lists) of integers/floats"
 
+    if type(matrix) is not list:
+        raise TypeError(y)
+
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    if type(matrix) is not list:
-        raise TypeError(y)
 
     for row in matrix:
         new_list = []
