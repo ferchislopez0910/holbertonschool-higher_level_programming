20fbd91f6fd0755ac8f49caab4d9de74aa88f3b1
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-06-01 18:10:30 -0500

    Update 1: task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,5 +5,5 @@ function that returns the list of available attributes and methods of an object
 
 
 def lookup(obj):
-    # Funcion that retorn dir of obj
+    """ Funcion that retorn dir of obj"""
     return(dir(obj))
