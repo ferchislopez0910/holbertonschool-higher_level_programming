18745401a955760c8800e7cbaf578abfd0ee43f4
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-06-01 21:39:10 -0500

    Update 2: correct if statement

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Class base geo"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
