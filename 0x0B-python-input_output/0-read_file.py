5b1a33c37fb45acda98c9d6b406dbf645afbece1
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-06-02 16:51:50 -0500

    Update 1: pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Write a function that reads a text file"""
 
+
 def read_file(filename=""):
     """Write a function that reads a text file"""
-    with open(filename, encoding = 'utf-8') as f:
+
+    with open(filename, encoding='utf-8') as f:
         content = f.read()
         print(content, end='')
