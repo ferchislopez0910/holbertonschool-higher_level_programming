4226cb03d164cab6d73996ce6292b28ec33954ed
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-06-08 18:13:36 -0500

    Update 4: 8/06 6:13

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,6 +3,7 @@
 
 
 import unittest
+from models.base import Base
 from models.rectangle import Rectangle
 
 
@@ -17,18 +18,18 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.y, 0)
 
         r2 = Rectangle(2, 10)
-        self.assertEqual(r1.id, 2)
-        self.assertEqual(r1.width, 2)
-        self.assertEqual(r1.height, 10)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
 
         r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r1.id, 12)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
+        self.assertEqual(r3.id, 12)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
 
     def test_Raises(self):
         r1 = Rectangle("10", 2)
