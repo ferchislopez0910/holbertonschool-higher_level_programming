9a0242d7ac1ac48dec86320a29041413a28e4188
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-14 00:53:06 +0000

    created

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,2 @@
 -- Write a script that creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXISTS force_name(
-	id INT,
-	name VARCHAR(256) IS NOT null);
-INSERT INTO force_name (id, name) VALUES (89, 'Holberton School');
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256));
