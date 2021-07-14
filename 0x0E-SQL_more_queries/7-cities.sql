7aee1d944e952faefa2e22dad0083051a368c121
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-14 02:54:02 +0000

    udpate correct task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,8 @@
--- 
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `state_id` INT NOT NULL AUTO_INCREMENT,
-    `name ` VARCHAR(256) NOT NULL,
-    UNIQUE (id),
-    PRIMARY KEY(`id`),
-    FOREIGN KEY (id, states)
+-- creates the database hbtn_0d_usa and the table cities 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+	name VARCHAR(256) NOT NULL
 );
