1460cee1aface187145ee8f4e0e366c30ba81fca
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-14 02:19:32 +0000

    Update 6 task!

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Write a script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (id INT NOT NULL AUTO_INCREMENT,'name' VARCHAR(256) NOT NULL, PRIMARY KEY('id'));
