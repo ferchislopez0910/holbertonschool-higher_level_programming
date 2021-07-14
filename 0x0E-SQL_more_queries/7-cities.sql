8d73eab2bbc1dd6fc49410dbc08f5125f8a25050
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-14 02:49:09 +0000

    correct unique

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-    PRIMARY KEY(`id`),
     `id` INT NOT NULL AUTO_INCREMENT,
     `state_id` INT NOT NULL AUTO_INCREMENT,
-    `state_id` VARCHAR(256) NOT NULL,
+    `name ` VARCHAR(256) NOT NULL,
+    UNIQUE (id),
+    PRIMARY KEY(`id`),
     FOREIGN KEY (id, states)
 );
