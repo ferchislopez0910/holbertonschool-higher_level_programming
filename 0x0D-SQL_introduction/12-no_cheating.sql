00b83c5a4a5b277fd4dd1a3acac36a8127c5ef13
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-15 20:13:55 -0500

    Update project

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Write a script that updates the score of Bob to 10 in the table second_table.
+UPDATE second_table name SET score = 10 WHERE score = 14;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Write a script that creates a table second_t
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
