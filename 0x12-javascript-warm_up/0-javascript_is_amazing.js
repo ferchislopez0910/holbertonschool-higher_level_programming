38c366edc3401c7b0536c299c53c41ea8700633a
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-02 17:02:38 -0500

    Update!

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-//rite a script that prints “JavaScript is amazing”
+// Write a script that prints “JavaScript is amazing”//
+const myVar = 'JavaScript is amazing';
 
-myVar = console.log("JavaScript is amazing");
+console.log(myVar);
