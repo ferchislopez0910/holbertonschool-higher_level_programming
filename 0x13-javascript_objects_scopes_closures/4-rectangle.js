e731687c130f08186b095869e89532f655d09fae
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-03 16:33:53 -0500

    Update task 4!

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// Create an instance method called print() that prints the rectangle using the character X
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let rect = "";
+      for (let j = 0; j < this.width; j++) {
+        rect += "X";
+      }
+      console.log(rect);
+    }
+  }
+  rotate() {
+    let saveVale = this.height;
+    this.height = this.width;
+    this.width = saveVale;
+  }
+  double() {
+	this.height = this.height * 2;
+	this.width = this.width * 2;
+  }
+
+}
+module.exports = Rectangle;
