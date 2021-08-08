7a957e48faaf9172d667c381ec2a1f2bedfb9608
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-08 02:53:21 +0000

    update: task 2 ok!

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""cript that lists all states from the database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        search = sys.argv[4]
+        server = "localhost"
+        """Open database connection"""
+        conn = MySQLdb.connect(host=server, port=3306, user=username,
+                               passwd=password, db=database, charset="utf8")
+
+        """ prepare a cursor object using cursor() method"""
+        cur = conn.cursor()
+        """ execute SQL query using execute() method."""
+        cur.execute(
+            "SELECT * FROM states WHERE name = '%s' ORDER BY id ASC" % search)
+
+        """Fetch a single row using fetchone() method."""
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+
+        """disconnect from server"""
+        cur.close()
+        conn.close()
+
+    else:
+        print("Error - Introduce los argumentos correctamente")
