284650b213112bf2e0856487515f01dfa657747c
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-08 02:23:05 +0000

    correct: pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,27 +5,27 @@ import sys
 import MySQLdb
 
 if len(sys.argv) == 4:
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database = sys.argv[3]
-	"""Open database connection"""
-	conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-						   passwd=password, db=database, charset="utf8")
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    """Open database connection"""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
 
-	""" prepare a cursor object using cursor() method"""
-	cur = conn.cursor()
-	""" execute SQL query using execute() method."""
-	cur.execute(
-		"SELECT * FROM states ORDER BY id ASC")  # HERE I have to know SQL to grab all states in my database
+    """ prepare a cursor object using cursor() method"""
+    cur = conn.cursor()
+    """ execute SQL query using execute() method."""
+    cur.execute(
+        "SELECT * FROM states ORDER BY id ASC")
 
-	"""Fetch a single row using fetchone() method."""
-	query_rows = cur.fetchall()
-	for row in query_rows:
-		print(row)
+    """Fetch a single row using fetchone() method."""
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
 
-	"""disconnect from server"""
-	cur.close()
-	conn.close()
+    """disconnect from server"""
+    cur.close()
+    conn.close()
 
 else:
-	print("Error - Introduce los argumentos correctamente")
+    print("Error - Introduce los argumentos correctamente")
