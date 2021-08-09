4ee3aac47cc3b245c1d352f51f5460f5cdc39b97
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-09 03:11:00 +0000

    correct pep8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,9 +9,11 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
 
 """Its a class of lib Alchemist """
+
+
 class City(Base):
     """Class that to created table of bd"""
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
