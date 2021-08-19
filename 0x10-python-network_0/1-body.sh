4b897d0e3ca535e6d15c5e9f53b7f3bd2c3f138e
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-19 16:39:59 -0500

    correct task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# ara hacer esto, primero tendríamos que solicitar los encabezados de una respuesta y luego extraer el código de respuesta. Así es como se vería.
-curl $1 -I -o headers -s
+# ara hacer esto, primero tendríamos que solicitar los encabezados 
+# de una respuesta y luego extraer el código de respuesta. Así es como se vería.
+curl $1 -I -o headers | head -n 1 | cut '-d ' '-f2'
