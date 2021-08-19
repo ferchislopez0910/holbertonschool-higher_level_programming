ebe2571cc69675ee070ec5f7f1879706ccc9b36b
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-19 16:32:33 -0500

    correct commits

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Este comando lo que hace es 
+# Este comando lo que hace es
+# I es para mostrar solo encabezados -s corregir errores
 curl -sI $1 | grep -i Content-Length | awk '{print $2}'
